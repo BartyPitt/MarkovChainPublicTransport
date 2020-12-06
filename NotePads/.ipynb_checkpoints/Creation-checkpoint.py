f195350ed0aef7d39e46e52e3137e53cc345b52f
Barty <bartypitt@gmail.com>
2020-12-06 15:08:17 +0000

    start of day commit

--- NotePads/.ipynb_checkpoints/Creation-checkpoint.py
+++ NotePads/.ipynb_checkpoints/Creation-checkpoint.py
@@ -1,16 +1,14 @@
 import numpy as np
 import random
-import networkx as nx
-import matplotlib.pyplot as plt
 """
 What we are doing is creating a set of base nodes and then we will sub divide
 the arcs from  to form a more complex fake network that roughly mimics a real network.
 Each of these base nodes represent a change over between routes, and is connected to atleast 3 other base nodes.
 """
-NumberOfBaseNodes = 7
+NumberOfBaseNodes = 6
 np.random.seed(5)
 BaseMatrix = np.zeros((NumberOfBaseNodes,NumberOfBaseNodes) , dtype=int)
-ConectionVector = np.random.randint(0,10,(NumberOfBaseNodes))
+ConectionVector = np.random.randint(0,2,(NumberOfBaseNodes))
 print(ConectionVector)
 
 """
@@ -32,7 +30,7 @@ for i , n in enumerate(ConectionVector):
     PickableNodes = RemainingNodes & SetNotContainingNode #do this better you know its one function , atleast try and find it before just commenting it and ignoring it 
     KeyNode = random.choice(list(PickableNodes)) # selects a node thats not itself.
     KeyNodes = [KeyNode]
-    if n > 1:
+    if n == 1:
         KeyNodes.append(random.choice(list(SetNotContainingNode)))
     for Node in KeyNodes:
         BaseMatrix[PreviousNode,Node] = 1
@@ -40,16 +38,16 @@ for i , n in enumerate(ConectionVector):
     PreviousNode = KeyNode
     RemainingNodes.remove(KeyNode)
 
-RandomNumberMatrix = np.random.randint(3,5 , size = (NumberOfBaseNodes, NumberOfBaseNodes))
+RandomNumberMatrix = np.random.randint(1,5 , size = (NumberOfBaseNodes, NumberOfBaseNodes))
 CondencedMatrix = RandomNumberMatrix * BaseMatrix #this is a matrix that represents the number of nodes between any of the big nodes.
 TotalNumberOfNodes = np.sum(CondencedMatrix) - np.sum(BaseMatrix) + NumberOfBaseNodes
 
 print(CondencedMatrix)
 print(TotalNumberOfNodes)
 
-BigMatrix = np.zeros((TotalNumberOfNodes, TotalNumberOfNodes) , dtype = int)
+BigMatrix = np.zeros((TotalNumberOfNodes, TotalNumberOfNodes))
 CurrentRow = NumberOfBaseNodes
-Indexer = 1
+
 for j,Row in enumerate(CondencedMatrix):
     for  i , Point in enumerate(Row):
 
@@ -57,53 +55,25 @@ for j,Row in enumerate(CondencedMatrix):
         if Point == 0:
             continue
         elif Point == 1:
-            BigMatrix[j][i] = Indexer
-            Indexer += 1 
+            BigMatrix[j][i] = 1 
             continue
 
         else: #If the nodes are not neighbors connect to the neighbor node
-            BigMatrix[j][CurrentRow] = Indexer
-            Indexer += 1         
+            BigMatrix[j][CurrentRow] = 1           
 
         #If there is a chain to be formed
         while Point > 1:
             if Point == 2:
-                BigMatrix[CurrentRow][i] = Indexer
+                BigMatrix[CurrentRow][i] = 1
                 Point = 0
             else:
-                BigMatrix[CurrentRow][CurrentRow + 1] = Indexer
+                BigMatrix[CurrentRow][CurrentRow + 1] = 1
                 Point -= 1
             CurrentRow += 1
-            Indexer += 1
-
-AdjacentDuelMatrix = np.zeros((Indexer,Indexer) , dtype = int)
-
-for y , Row in enumerate(BigMatrix):
-    for x , Point in enumerate(Row):
-        if Point != 0:
-            for Arc in BigMatrix[x]:
-                if Arc != 0:
-                    AdjacentDuelMatrix[Point][Arc] = 1
-
-
-
-a = nx.convert_matrix.from_numpy_matrix(BaseMatrix)
-nx.draw_networkx(a)
-plt.show()
-
-a = nx.convert_matrix.from_numpy_matrix(BigMatrix)
-nx.draw_networkx(a)
-plt.show()
 
 print(CondencedMatrix)
 print(" ")
 print(BigMatrix)
-print(" ")
-print(AdjacentDuelMatrix)
-
-Important 
-
-
 
 #Now for calculating the duel of that matrix
 #For each node 
