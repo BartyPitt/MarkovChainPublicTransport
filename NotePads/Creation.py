a81ef59592dfb72a85510f6a73d4b5c60f00453e
Barty <bartypitt@gmail.com>
2020-12-07 10:12:39 +0000

    got it to work for the waiting graph

--- NotePads/Creation.py
+++ NotePads/Creation.py
@@ -10,7 +10,7 @@ Each of these base nodes represent a change over between routes, and is connecte
 NumberOfBaseNodes = 7
 np.random.seed(5)
 BaseMatrix = np.zeros((NumberOfBaseNodes,NumberOfBaseNodes) , dtype=int)
-ConectionVector = np.random.randint(0,10,(NumberOfBaseNodes))
+ConectionVector = np.random.randint(2,4,(NumberOfBaseNodes))
 print(ConectionVector)
 
 """
@@ -40,12 +40,12 @@ for i , n in enumerate(ConectionVector):
     PreviousNode = KeyNode
     RemainingNodes.remove(KeyNode)
 
-RandomNumberMatrix = np.random.randint(3,5 , size = (NumberOfBaseNodes, NumberOfBaseNodes))
+RandomNumberMatrix = np.random.randint(1,5 , size = (NumberOfBaseNodes, NumberOfBaseNodes))
 CondencedMatrix = RandomNumberMatrix * BaseMatrix #this is a matrix that represents the number of nodes between any of the big nodes.
 TotalNumberOfNodes = np.sum(CondencedMatrix) - np.sum(BaseMatrix) + NumberOfBaseNodes
 
-print(CondencedMatrix)
-print(TotalNumberOfNodes)
+#print(CondencedMatrix)
+#print(TotalNumberOfNodes)
 
 BigMatrix = np.zeros((TotalNumberOfNodes, TotalNumberOfNodes) , dtype = int)
 CurrentRow = NumberOfBaseNodes
@@ -86,22 +86,82 @@ for y , Row in enumerate(BigMatrix):
                     AdjacentDuelMatrix[Point][Arc] = 1
 
 
+def DisplayMatrix(MatrixToBeDisplayed):
+    a = nx.convert_matrix.from_numpy_matrix(MatrixToBeDisplayed , create_using=nx.MultiDiGraph)
+    nx.draw_networkx(a)
+    plt.show()
+    return True
 
-a = nx.convert_matrix.from_numpy_matrix(BaseMatrix)
-nx.draw_networkx(a)
-plt.show()
+DisplayMatrix(BaseMatrix)
+DisplayMatrix(BigMatrix)
 
-a = nx.convert_matrix.from_numpy_matrix(BigMatrix)
-nx.draw_networkx(a)
-plt.show()
 
-print(CondencedMatrix)
+"""
+Lets think about what to do next.
+What we want is a set of matrices that contain all of the fun pieces of infomation.
+Plan. Multiply the matrix by a random number matrix , then row stocasitc normalise.
+"""
+BigMatrix = np.column_stack((BigMatrix , [1 for i in range(BigMatrix.shape[0])]))
+BigMatrix = np.vstack((BigMatrix , [1 for i in range(BigMatrix.shape[1])]))
+
+
+def MakeRowstochastic(InputMatrix):
+    """
+    Takes in a matix and makes it row stochastic
+    Matrix must be a numpy matrix.
+    TODO figure out if this function is functionally pure
+    """
+    AverageVector = np.sum(InputMatrix , axis = 1) # Find the row average
+    Output = InputMatrix  / AverageVector[: , None]  #divide each of the rows by the row average.
+    return Output
+
+WaitingMatrix = np.random.rand(*BigMatrix.shape) * BigMatrix
+WaitingMatrix = MakeRowstochastic(WaitingMatrix)
+
+def WaitingTimeDistributionFunction(input = None):
+    """
+    Function for generating the numbers for the waiting times
+    Takes a nymber returns 1-x/x of that number
+    If no number is generated then it picks a number between 10 - 30
+    """
+    if input == None:
+        input = random.randrange(start = 2 , stop = 20)
+    return (input - 1) / input
+
+
+for i in range(len(WaitingMatrix)):
+    WaitingMatrix[i][i] = WaitingTimeDistributionFunction()
+
+WaitingMatrix[-1][-1] = 0
+WaitingMatrix = MakeRowstochastic(WaitingMatrix)
+#print(WaitingMatrix)
+
+
+
+w , v = np.linalg.eig(np.transpose(WaitingMatrix))
+#print(w) 
 print(" ")
-print(BigMatrix)
-print(" ")
-print(AdjacentDuelMatrix)
+#print(v)
 
-Important 
+AddInfititum = np.linalg.matrix_power(WaitingMatrix , 60)
+print("large numbers")
+print(AddInfititum)
+a = np.zeros((len(WaitingMatrix)))
+a.fill(1)
+print("A is ")
+print(a)
+b = np.matmul(a,AddInfititum)
+print(b)
+
+for i , Point in enumerate(w):
+    #yo we got the https://0.30000000000000004.com/ joke. so this is a crude way of kinda dealing with that
+    if not np.iscomplex(Point) :
+        print(v[:,i])
+        plt.plot(v[:,i] , "ro")
+        plt.title(Point)
+        plt.show()
+        pass
+
 
 
 
